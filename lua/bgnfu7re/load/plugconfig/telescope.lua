5f14f9282c89b6a980f8b7c717d0574a611826fe
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-08-18 11:21:43 +0300

    upd config

--- lua/bgnfu7re/load/plugconfig/telescope.lua
+++ lua/bgnfu7re/load/plugconfig/telescope.lua
@@ -4,25 +4,31 @@ if not ok then
 end
 
 telescope.setup({
-  pickers = {
-    buffers = {
-      theme = 'dropdown',
-    },
-
-    live_grep = {
-      theme = 'dropdown',
-    },
-
-    find_files = {
-      theme = 'dropdown',
-    },
-
-    commands = {
-      theme = 'dropdown',
-    },
-
-    lsp_dynamic_workspace_symbols = {
-      theme = 'dropdown',
-    },
+  defaults = {
+    sorting_strategy = "ascending",  -- display results top->bottom
+    layout_config = {
+      prompt_position = "top"  -- search bar at the top
+    }
   },
+  -- pickers = {
+  --   buffers = {
+  --     theme = 'dropdown',
+  --   },
+
+  --   live_grep = {
+  --     theme = 'dropdown',
+  --   },
+
+  --   find_files = {
+  --     theme = 'dropdown',
+  --   },
+
+  --   commands = {
+  --     theme = 'dropdown',
+  --   },
+
+  --   lsp_dynamic_workspace_symbols = {
+  --     theme = 'dropdown',
+  --   },
+  -- },
 })
