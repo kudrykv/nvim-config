f40de4e3f015f7e64c7a5b89d5c736b1c84572c4
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-07-21 16:55:52 +0300

    upd

--- lua/bgnfu7re/keymap.lua
+++ lua/bgnfu7re/keymap.lua
@@ -2,10 +2,13 @@
 vim.g.mapleader = ' '
 
 
+-- GENERAL ---------------------------------------
 vim.keymap.set({ 'n' }, 'x', '"_x', { desc = 'Remove one char w/o copying it' })
 
--- file tree
+-- FILE TREE -------------------------------------
 vim.keymap.set('n', '<M-1>', '<CMD>NvimTreeToggle<CR>', { desc = '(⌥+F1) Toggle file tree' })
+
+-- FILE NAVIGATION -------------------------------
 
 -- GIT -------------------------------------------
 -- hunks
@@ -15,6 +18,7 @@ vim.keymap.set('n', '<Leader>hn', '<CMD>Gitsigns next_hunk<CR>', { desc = 'Next 
 vim.keymap.set('n', '<Leader>hp', '<CMD>Gitsigns prev_hunk<CR>', { desc = 'Prev hunk' })
 
 vim.keymap.set('n', '<Leader>gb', '<CMD>Gitsigns blame<CR>', { desc = 'Blame' })
+vim.keymap.set('n', '<C-k>', '<CMD>Git<CR>', { desc = 'Git (pre)Commit' })
 vim.keymap.set('n', '<Leader>gc', '<CMD>Git<CR>', { desc = 'Git -> (pre)Commit' })
 
 -- LSP -------------------------------------------
